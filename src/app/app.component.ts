d09f10c3846608da82682ab4808e70b61b41f5f4
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-22 00:10:51 -0500

    Se establecieron los estilos para el background, textos, y se implemento el PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PipesApp';
+
+  nombre:string = 'Alejandro';
+  valor: number = 1000;
+  obj = {
+    nombre:'Fernando'
+  }
+
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
