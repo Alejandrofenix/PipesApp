0e9e2d6d34451c1c8bd5be258f94b2b7cd4dddd0
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-26 15:56:12 -0500

    Se agregó el pipe async

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component,  } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -62,5 +63,12 @@ export class NoComunesComponent{
 ]
 
 
+//Async pupe
+miObservable = interval(1000);
+
+valorPromesa = new Promise((resolve,reject)=>{
+  setTimeout(()=>
+  resolve('Tenemos data de promesa'),3500);
+});
 
 }
