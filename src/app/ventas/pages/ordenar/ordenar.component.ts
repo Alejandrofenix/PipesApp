0be520b821742bc81ecf1a337369c2e6aaeb0971
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-26 16:54:49 -0500

    Se agregó el pipe personalizado para mayusculas y minusculas

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class OrdenarComponent implements OnInit {
-
+  enMayusculas: boolean =true;
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  change(){
+    this.enMayusculas=!this.enMayusculas;
+  }
 }
